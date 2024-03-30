721cd05388f1320d1ef55b493c641223533bb9b5
jarin3956 <jarinmeethal@gmail.com>
2024-03-30 12:37:38 +0530

    socket upd

--- Socket/Socket.js
+++ Socket/Socket.js
@@ -6,7 +6,6 @@ function createGroupChatRoom(friendIds) {
     const concatenatedString = friendIds.join('-'); 
     const hash = crypto.createHash('sha1').update(concatenatedString).digest('hex');
     const chatRoomId = hash.substring(0, 20);
-    console.log('Chat room id is', chatRoomId);
     return chatRoomId;
 }
 
@@ -15,13 +14,20 @@ function initializeSocket(server) {
     const connectedUsers = new Set();
 
     io.on('connection', (socket) => {
-        console.log(`User connected: ${socket.id}`);
-        connectedUsers.add(socket.id);
+        console.log(`User socketid: ${socket.id}`);
+        // connectedUsers.add(socket.id);
+        socket.on('set-up', (userId) => {
+            console.log(`User ${userId} connected to chat`);
+            socket.join(userId);
+            socket.emit('chat-connected');
+        });
+
         socket.on('addFriendToChat', ({ friendId }) => {
             console.log(`User added: ${friendId}`)
         });
         socket.on('startGroupChat', ({ friendIds }) => {
             const chatRoomId = createGroupChatRoom(friendIds);
+            console.log('Chat room id emit', chatRoomId);
             friendIds.forEach((friendId) => {
                 io.to(friendId).emit('groupChatStarted', { chatRoomId });
             });
